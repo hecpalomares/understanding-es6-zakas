e96b2aab6b9f80b799191b9df97a0846339d0566
hecpalomares <hpalomares12@gmail.com>
2018-03-25 16:02:46 -0600

    Chapter 3: Spread operator, Name property, Purpose of the Function, Block-Level function, Arrow Functions (1/2).

--- chapters/chapter-3-functions.js
+++ chapters/chapter-3-functions.js
@@ -47,3 +47,91 @@ let monterreyFC = new Team("Monterrey FC", "J. Basanta", " R. Funes", " A. Hurta
 console.log(monterreyFC.getTeamInfo());
 
 // Rest parameter has two restrictions. 1. Only one rest parameter (...) per function. 2. The rest parameter should be the last to appear.
+
+/* 	3. Spread Operators */
+//	Specifies an array to be splitted in as separate arguments to a function.
+let values = [24, 15, 66, 78, 92, 122];
+console.log(Math.max(...values));
+
+/*	4. Name property */
+//	All functions in ES6 have a 'name' property, to print the name of a function. Useful to debug stack traces and anonymous functions.
+
+function randomFunction() {
+	return 'x';
+}
+
+let doSomethingOld = function () {
+	return 'y';
+}
+
+let doSomethingNew = function newFunction() {
+	return 'z';
+}
+
+console.log(randomFunction.name);			// randomFunction: function declaration, directly the name of the function that was declarated
+console.log(doSomethingOld.name);			// doSomethingOld: function expression of anonymous function, the name to the matched variable
+console.log(doSomethingNew.name);			// newFunction: 	 function expression, the name of the named function
+
+/*	5. Dual Purpose of Functions */
+
+// Capitalization of a function is only a indication that needs to be called with a 'new' keyword.
+function Person(name) {
+	this.name = name;
+}
+
+// When used with 'new' keyword, 'this' value is the new object which is returned.
+let person = new Person("Cassius");
+let notAPerson = Person("Cassius");
+
+console.log(person);			// Person { name: Cassius }
+console.log(notAPerson);	// undefined
+
+// Functions have a [Call] and [Constructor] method. [Call] method executes the code as its in the body function. When called with [Constructor]
+// the method returns an new object (called an instance), and with 'this' set to the instnace. Arrow functions do not have a [Constructor] method.
+
+/* 6. Block-Level Functions */
+// Functions declarations inside a block (for example and if) are best to be avoided. The best practice is to use a function expression.
+
+if(true) {
+	// Syntax error in ES5, not in ES6
+	function myBlockedFunc() {
+		return 'x';
+	}
+}
+
+console.log(myBlockedFunc());	// x
+
+//	Function expressions are not hoisted to the top of the block. Line 106 throws an error becouse the function it is not yet declared.
+if(true) {
+	// console.log(typeof myBlockedFunc2);	// error, is not defined
+
+	let myBlockedFunc2 = function() {
+		return 'y';
+	}
+
+	console.log(myBlockedFunc2());		// y
+
+}
+
+console.log(typeof myBlockedFunc2);	// undefined
+
+//	Function declarations are hoisted to the top of the containing function or the global enviroment.
+if(true) {
+	console.log(typeof myBlockedFunc3);	// "function"
+
+	function myBlockedFunc3() {
+		return 'z';
+	}
+
+	console.log(myBlockedFunc3());		// z
+
+}
+
+console.log(typeof myBlockedFunc3);	// // "function"
+
+/* 7. Arrow Functions */
+// 	1. Not this, super and arguments bindings: The value of this keywords are defined by the closest containing non-arrow function.
+// 	2. Cannot be called with new: Arrow functions do not have [Construct] method.
+//  3. No prototype: Since 'new' keyword cannot be used, there's no need of prototype.
+// 	4. Can't change 'this': It remaisn the same throughout the entire life cycle.
+// 	5. No arguments object: No arguments binding, rely on named and rest parameters.
