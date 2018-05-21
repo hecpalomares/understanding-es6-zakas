e4dc043f2b808af0ada21ed3e1f20d9cd438d946
hecpalomares <hpalomares12@gmail.com>
2018-05-20 22:11:36 -0500

    Chapter 12: Preventing Property Deletion, Calling constructors without new, Override Abstract Base class. Summary.

--- chapters/chapter-12-proxies.js
+++ chapters/chapter-12-proxies.js
@@ -46,7 +46,7 @@ console.log(target2.name);  // target 2
 
 // proxy2.anotherNameProp = "Random Property"; // TypeError: Property must be a number
 
-// Object shape Validation using the 'get' Trap
+// Object shape validation using the 'get' Trap
 // 'get' trap intercepts property read operations
 let proxy3 = new Proxy({}, {
   get(trapTarget, key, receiver) {
@@ -60,3 +60,72 @@ let proxy3 = new Proxy({}, {
 proxy3.name = "Proxy 3 Name";
 console.log(proxy3.name);        // Proxy 3 Name
 // console.log(proxy3.lastname);   // TypeError: Property: lastname doesn't exist
+
+// Preventing Property Deletion
+let person = {
+  name: "Hector",
+  age: 25
+};
+
+let proxy4 = new Proxy(person, {
+  deleteProperty(person, key) {
+    if(key === "name") {
+      return false;
+    } else {
+      return Reflect.deleteProperty(person, key);
+    }
+  }
+});
+
+console.log("name" in person);
+
+let resultAgeProp = delete proxy4.age;
+console.log(resultAgeProp); // true
+
+let resultNameProp = delete proxy4.name;
+console.log(resultNameProp); // false
+
+console.log(proxy4);    // { name: 'Hector' }
+
+// Calling Constructors Without new
+function Numbers(...values) {
+  if(typeof new.target === "undefined"){
+    throw new TypeError("This function must be called with new.");
+  }
+  this.values = values;
+}
+
+let NumbersProxy = new Proxy(Numbers, {
+  apply: function(trapTarget, thisArg, argumentList) {
+    return Reflect.construct(trapTarget, argumentList);
+  }
+});
+
+let instanceNumbers = NumbersProxy(1, 2, 3, 4);
+console.log(instanceNumbers.values); //  [1, 2, 3, 4];
+// NumbersProxy() function allows to call Numbers() without using new. Apply trap calls Reflect.construct.
+
+// Overriding Abstract Base Class Constructors
+class AbstractNumbers {
+  constructor(...values) {
+    if(new.target === AbstractNumbers) {
+      throw new TypeError("This function must be inherited");
+    }
+    this.values = values;
+  }
+}
+
+// The AbstractNumbersProxy uses the construct trap to intercept the call to the new AbstractNumbersProxy() method.
+let AbstractNumbersProxy = new Proxy(AbstractNumbers, {
+  construct: function(trapTarget, argumentList) {
+    return Reflect.construct(trapTarget, argumentList, function() {});
+  }
+});
+
+let instanceNumbers2 = new AbstractNumbersProxy(1, 2, 3, 4);
+console.log(instanceNumbers2.value);  // undefined
+
+// Summary
+// Proxies on a object intercept the calls to the object, and run certain logic on each trap triggered
+// Reflection API: allows to implement the default behavior for each proxy trap
+// Only the get, set, and has proxy traps will ever be called on a proxy used as a property
